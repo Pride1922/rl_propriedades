33635482a32520d2c56cde3be9c0e48257b2b605
Pride1922 <48358139+Pride1922@users.noreply.github.com>
2021-02-09 11:04:51 +0100

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -4,13 +4,24 @@ games {'gta5' }
 author 'Pride1922'
 description 'Redline Properties system'
 version '1.0.0'
+
 server_scripts {
 	'@mysql-async/lib/MySQL.lua',
 	'@async/async.lua',
 	'server/server.lua',
-	'server/armario.lua'
 }
 
 client_scripts{
 	'client/client.lua'
+}
+
+dependencies {
+	'es_extended',
+	'esx_addonaccount',
+	'esx_addoninventory',
+	'esx_datastore',
+	'esx_skin',
+	'skinchanger',
+	'esx_inventoryhud',
+	'mythic_notify'
 }
